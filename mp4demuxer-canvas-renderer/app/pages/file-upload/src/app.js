484eab3315177de6baf5114311ec09c04672138c
Alyson F Souza <alysonforever@gmail.com>
2023-10-26 19:48:56 -0300

    criando camada view do app

---
+++ mp4demuxer-canvas-renderer/app/pages/file-upload/src/app.js
@@ -0,0 +1,23 @@
+import Clock from './deps/clock.js';
+import View from './view.js';
+
+const clock = new Clock()
+const view = new View()
+
+let took = ''
+
+view.configureOnFileChange(file => {
+    clock.start((time) => {
+        took = time;
+        view.updateElapsedTime(`Process started ${time}`)
+    })
+
+    setTimeout(() => {
+        clock.stop()
+        view.updateElapsedTime(`Process took ${took.replace('ago', '')}`)
+    }, 5000)
+})
+
+
+
+
