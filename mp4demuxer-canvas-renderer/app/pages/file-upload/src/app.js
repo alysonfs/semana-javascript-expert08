2c3b031cf5d6e9b72fbc9419bf5b35dd1b2dcc82
Alyson F Souza <alysonforever@gmail.com>
2023-10-26 20:01:30 -0300

    test: add fake fetch

--- mp4demuxer-canvas-renderer/app/pages/file-upload/src/app.js
+++ mp4demuxer-canvas-renderer/app/pages/file-upload/src/app.js
@@ -18,6 +18,26 @@ view.configureOnFileChange(file => {
     }, 5000)
 })
 
+// Carrega automaticamente o video
+async function fakeFetch () {
+    const filePath = '/videos/frag_bunny.mp4'
 
+    const response = await fetch(filePath)
+    // Captura o tamanho do video
+    // const response = await fetch(filePath, {
+    //     method: 'HEAD'
+    // })
+    // const sizeVideo = response.headers.get('content-length')
+    // debugger
 
+    const file = new File([await response.blob()], filePath, {
+        type: 'video/mp4',
+        lastModified: new Date()
+    })
 
+    const event = new Event('change')
+    Reflect.defineProperty(event, 'target', { value: { files: [file] } })
+    document.getElementById('fileUpload').dispatchEvent(event)
+}
+
+fakeFetch()
